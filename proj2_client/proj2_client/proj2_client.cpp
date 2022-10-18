f35b6d1fe736d85793653605890e21dd5aa3a527
wq <wqianz@163.com>
2022-10-18 17:08:25 +0800

    first add: basic send&recv from test files

---
+++ proj2_client/proj2_client/proj2_client.cpp
@@ -0,0 +1,68 @@
+﻿// proj2_client.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+// client.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+#include <windows.h>
+#include <winsock.h>
+#include <stdio.h>
+//#include <stdlib.h>
+#define SERVER_PORT 0x1234
+
+int main(int argc, char* argv[])
+{
+	SOCKET sock;
+	struct sockaddr_in server;
+	char* buf;
+	int len;
+	WSAData wsa;
+	int val;
+	int count = 0;
+
+	WSAStartup(0x101, &wsa);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) {
+		printf("Creating socket error\n");
+		return 0;
+	}
+
+
+	server.sin_family = AF_INET;
+	server.sin_addr.S_un.S_addr = htonl(INADDR_LOOPBACK);//inet_addr("202.115.12.35");
+	server.sin_port = htons(SERVER_PORT);
+
+	if (connect(sock, (sockaddr*)&server, sizeof(server)) < 0) {
+		val = WSAGetLastError();
+		printf("Conneting to server error\n");
+		return 0;
+	}
+
+	printf("connect to server\n");
+
+	buf = (char*)malloc(2048);
+	//scan the keboard until input string is "exit"
+	while (1) {
+			printf("Send = ");
+			scanf_s("%s", buf, 2048);
+			if (strcmp(buf, "exit") == 0)
+				break;
+			send(sock, buf, strlen(buf), 0);
+			
+		val = recv(sock, buf, 100, 0);//recv返回buf长度大小
+		if (val > 0) {
+			//buf[val] = 0;//send处注意+1，否则就在这手动加终止符
+			printf("%s\n", buf);
+		}
+		else
+			break;
+	}
+
+	//close the socket immediately
+	closesocket(sock);
+	WSACleanup();
+	free(buf);
+	printf("passed\n");
+	return 0;
+}
---
+++ proj2_server/proj2_server/proj2_server.cpp
@@ -0,0 +1,144 @@
+﻿// proj2_server.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+// server.cpp : 
+//
+
+#include "winsock.h"
+#include "stdio.h"
+//#pragma comment (lib,"wsock32.lib")
+struct socket_list {
+	SOCKET MainSock;
+	int num;
+	SOCKET sock_array[64];
+};
+void init_list(socket_list* list)
+{
+	int i;
+	list->MainSock = 0;
+	list->num = 0;
+	for (i = 0; i < 64; i++) {
+		list->sock_array[i] = 0;
+	}
+}
+void insert_list(SOCKET s, socket_list* list)
+{
+	int i;
+	for (i = 0; i < 64; i++) {
+		if (list->sock_array[i] == 0) {
+			list->sock_array[i] = s;
+			list->num += 1;
+			break;
+		}
+	}
+}
+void delete_list(SOCKET s, socket_list* list)
+{
+	int i;
+	for (i = 0; i < 64; i++) {
+		if (list->sock_array[i] == s) {
+			list->sock_array[i] = 0;
+			list->num -= 1;
+			break;
+		}
+	}
+}
+void make_fdlist(socket_list* list, fd_set* fd_list)
+{
+	int i;
+	FD_SET(list->MainSock, fd_list);
+	for (i = 0; i < 64; i++) {
+		if (list->sock_array[i] > 0) {
+			FD_SET(list->sock_array[i], fd_list);
+		}
+	}
+}
+int main(int argc, char* argv[])
+{
+	SOCKET s, sock;
+	struct sockaddr_in ser_addr, remote_addr;
+	int len;
+	char buf[128];
+	WSAData wsa;
+	int retval;
+	struct socket_list sock_list;
+	fd_set readfds, writefds, exceptfds;
+	timeval timeout;
+	int i;
+	unsigned long arg;
+
+	WSAStartup(0x101, &wsa);
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	if (s == SOCKET_ERROR)
+		return 0;
+	ser_addr.sin_family = AF_INET;
+	ser_addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	ser_addr.sin_port = htons(0x1234);
+	bind(s, (sockaddr*)&ser_addr, sizeof(ser_addr));
+
+	listen(s, 5);
+	timeout.tv_sec = 1;
+	timeout.tv_usec = 0;
+	init_list(&sock_list);
+	FD_ZERO(&readfds);
+	FD_ZERO(&writefds);
+	FD_ZERO(&exceptfds);
+	sock_list.MainSock = s;
+	arg = 1;
+	ioctlsocket(sock_list.MainSock, FIONBIO, &arg);
+	while (1) {
+		make_fdlist(&sock_list, &readfds);
+		//make_fdlist(&sock_list,&writefds);
+		//make_fdlist(&sock_list,&exceptfds);
+		retval = select(0, &readfds, &writefds, &exceptfds, NULL);
+		if (retval == SOCKET_ERROR) {
+			retval = WSAGetLastError();
+			break;
+		}
+		if (FD_ISSET(sock_list.MainSock, &readfds)) {
+			len = sizeof(remote_addr);
+			sock = accept(sock_list.MainSock, (sockaddr*)&remote_addr, &len);
+			if (sock == SOCKET_ERROR)
+				continue;
+			printf("accept a connection\n");
+			insert_list(sock, &sock_list);
+		}
+		for (i = 0; i < 64; i++) {
+			if (sock_list.sock_array[i] == 0)
+				continue;
+			sock = sock_list.sock_array[i];
+			if (FD_ISSET(sock, &readfds)) {
+				retval = recv(sock, buf, 128, 0);
+				if (retval == 0) {
+					closesocket(sock);
+					printf("close a socket\n");
+					delete_list(sock, &sock_list);
+					continue;
+				}
+				else if (retval == -1) {
+					retval = WSAGetLastError();
+					if (retval == WSAEWOULDBLOCK)
+						continue;
+					closesocket(sock);
+					printf("close a socket\n");
+					delete_list(sock, &sock_list);
+					continue;
+				}
+				buf[retval] = 0;
+				printf("->%s\n", buf);
+				send(sock, "ACK by server", 14, 0);
+			}
+			//if(FD_ISSET(sock,&writefds)){
+			//}
+			//if(FD_ISSET(sock,&exceptfds)){
+
+		}
+		FD_ZERO(&readfds);
+		FD_ZERO(&writefds);
+		FD_ZERO(&exceptfds);
+	}
+	closesocket(sock_list.MainSock);
+	WSACleanup();
+	return 0;
+}
+
+
